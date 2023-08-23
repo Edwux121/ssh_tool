e3b43a603546b71cbd9ab80eab9bfb9ca52d40c2
Edvinas Dryžas <edvinas.dryzas@teltonika.lt>
2023-08-23 12:38:23 +0300

    Added uci commands for Firewall rules when creating L2TP interface

--- ssh.py
+++ ssh.py
@@ -56,4 +56,43 @@ class SSH_connection:
         #Creating xl2tp interface
         xl2tp_server = f"uci add xl2tpd test51 && uci set xl2tpd.test51=service && uci set xl2tpd.test51.type='server' && uci set xl2tpd.test51.limit='{server_ip_limit}' && uci set xl2tpd.test51.localip='{server_ip_for_uci}' && uci set xl2tpd.test51.start='{server_ip_start}' && uci set xl2tpd.test51._name='test51' && uci set xl2tpd.test51.chap='0' && uci set xl2tpd.test51.enabled='1' && uci add xl2tpd login && uci set xl2tpd.@login[0].username='test' && uci set xl2tpd.@login[0].password='test12345' && uci set xl2tpd.@login[0].remoteip='{client_ip_str}' && uci commit"
 
-        
+        stdin, stdout, stderr = client.exec_command(xl2tp_server)
+
+        #Gathering current firewall last setting number
+        firewall_id = "uci show firewall | grep -Eo 'firewall\.([0-9]+)' | awk -F'.' '{print $2}' | tail -n 1"
+        stdin1, stdout1, stderr1 = client.exec_command(firewall_id)
+        highest_id = stdout1.read().decode().strip()
+
+        highest_id = int(highest_id) + 1
+        highest_id_str = str(highest_id)
+
+        #Adding new firewall settings for l2tp connection
+
+        #IDs for commands
+        new_id = highest_id_str
+        new_id_1 = highest_id + 1
+        new_id_2 = highest_id + 2
+        new_id_3 = highest_id + 3
+        new_id_4 = highest_id + 4
+        new_id_str = str(new_id)
+        new_id_1_str = str(new_id_1)
+        new_id_2_str = str(new_id_2)
+        new_id_3_str = str(new_id_3)
+        new_id_4_str = str(new_id_4)
+
+        command = f"uci set firewall.{new_id_str}=zone && uci set firewall.{new_id_str}.name='l2tp' && uci set firewall.{new_id_str}.device='l2tp+ xl2tp+' && uci set firewall.{new_id_str}.input='ACCEPT' && uci set firewall.{new_id_str}.forward='REJECT' && uci set firewall.{new_id_str}.masq='1' && uci set firewall.{new_id_str}.output='ACCEPT'"
+        stdin, stdout, stderr = client.exec_command(command)
+            
+        command1 = f"uci set firewall.{new_id_1_str}=forwarding && uci set firewall.{new_id_1_str}.dest='lan' && uci set firewall.{new_id_1_str}.src='l2tp'"
+        stdin2, stdou2, stderr2 = client.exec_command(command1)
+            
+        command2 = f"uci set firewall.{new_id_2_str}=forwarding && uci set firewall.{new_id_2_str}.dest='wan' && uci set firewall.{new_id_2_str}.src='l2tp'"
+        stdin3, stdout3, stderr3 = client.exec_command(command2)
+            
+        command3 = f"uci set firewall.{new_id_3_str}=forwarding && uci set firewall.{new_id_3_str}.dest='l2tp' && uci set firewall.{new_id_3_str}.src='lan'"
+        stdin4, stdout4, stderr4 = client.exec_command(command3)
+            
+        command4 = f"uci set firewall.{new_id_4_str}=rule && uci set firewall.{new_id_4_str}.dest_port='1701' && uci set firewall.{new_id_4_str}.src='wan' && uci set firewall.{new_id_4_str}.name='Allow-l2tp-traffic' && uci set firewall.{new_id_4_str}.target='ACCEPT' && uci set firewall.{new_id_4_str}.vpn_type='l2tp' && uci set firewall.{new_id_4_str}.proto='udp && uci set firewall.{new_id_4_str}.family='ipv4'"
+        stdin5, stdout5, stderr5 = client.exec_command(command4)
+
+        stdin6, stdout6, stderr6 = client.exec_command("uci commit")
